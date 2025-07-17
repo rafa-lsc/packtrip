0ea781c74f816957e2c897736703f81be4829deb
lucas <lucasmelo8328@gmail.com>
2025-07-17 08:22:12 -0300

    setup inicial do header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 import "./globals.css";
 
 const inter = Inter({
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable}  antialiased`}
       >
+        <Header/>
         {children}
       </body>
     </html>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return(
+    <div>
+        <h1>PackTrip</h1>
+        <ul>
+            <li>Minhas viagens</li>
+            <li>Mala Virtual</li>
+        </ul>
+    </div>
+    )
+}

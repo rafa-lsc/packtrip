70d01f4f3ad9ff32922626510f103363780e83e5
lucas <lucasmelo8328@gmail.com>
2025-07-29 19:43:18 -0300

    feat: toasts na pagina de mala virtual

--- src/app/mala-virtual/page.tsx
+++ src/app/mala-virtual/page.tsx
@@ -3,7 +3,10 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { listItemSchema, ListItemFormData } from "@/lib/formValidationSchemas/listItemSchema";
+import {
+  listItemSchema,
+  ListItemFormData,
+} from "@/lib/formValidationSchemas/listItemSchema";
 import { Plus, Search } from "lucide-react";
 import ItemList from "@/components/ItemList/page";
 import { girassol } from "@/lib/fonts";
@@ -14,6 +17,7 @@ import DeleteConfirmationModal from "@/components/DeleteItemConfirmationModal";
 
 import { malaAPI } from "@/lib/storageApi/mala";
 import type { ItemListType } from "@/lib/data";
+import { toast } from "sonner";
 
 export default function MalaVirtualPage() {
   const [items, setItems] = useState<ItemListType[]>([]);
@@ -21,8 +25,6 @@ export default function MalaVirtualPage() {
   const [deletingItem, setDeletingItem] = useState<ItemListType | null>(null);
   const [searchSentence, setSearchSentence] = useState("");
   const [filteredItems, setFilteredItems] = useState<ItemListType[]>([]);
-  
-
 
   const {
     register,
@@ -40,41 +42,57 @@ export default function MalaVirtualPage() {
       setItems(data);
     };
     fetchItems();
-    setFilteredItems(items)
+    setFilteredItems(items);
   }, []);
 
   useEffect(() => {
     setFilteredItems(
-      items.filter((item) =>
-        item.name.toLowerCase().includes(searchSentence.toLowerCase()) ||
-        item.category.toLowerCase().includes(searchSentence.toLowerCase()) ||
-        item.quantity.toString().includes(searchSentence.toLowerCase())
+      items.filter(
+        (item) =>
+          item.name.toLowerCase().includes(searchSentence.toLowerCase()) ||
+          item.category.toLowerCase().includes(searchSentence.toLowerCase()) ||
+          item.quantity.toString().includes(searchSentence.toLowerCase())
       )
     );
   }, [searchSentence, items]);
-  
 
   const onSubmit = (data: ListItemFormData) => {
-    const newItem: ItemListType = {
-      id: crypto.randomUUID(), //para id único
-      ...data,
-    };
-    malaAPI.create(newItem);
-    setItems(malaAPI.get());
-    reset();
+    try {
+      const newItem: ItemListType = {
+        id: crypto.randomUUID(), //para id único
+        ...data,
+      };
+
+      malaAPI.create(newItem);
+      setItems(malaAPI.get());
+      reset();
+      toast.success("Item criado com sucesso!");
+    } catch (error) {
+      toast.error("Erro ao criar item, tente novamente");
+    }
   };
 
   const handleEdit = (updatedItem: ItemListType) => {
-    malaAPI.update(updatedItem);
-    setItems(malaAPI.get());
-    setEditingItem(null);
+    try {
+      malaAPI.update(updatedItem);
+      setItems(malaAPI.get());
+      setEditingItem(null);
+      toast.success("Item editado com sucesso!");
+    } catch (error) {
+      toast.error("Erro ao editar item, tente novamente");
+    }
   };
 
   const handleDelete = () => {
     if (deletingItem) {
-      malaAPI.delete(deletingItem.id);
-      setItems(malaAPI.get());
-      setDeletingItem(null);
+      try {
+        malaAPI.delete(deletingItem.id);
+        setItems(malaAPI.get());
+        setDeletingItem(null);
+        toast.success("Item excluído com sucesso!");
+      } catch (error) {
+        toast.error("Erro ao excluir item, tente novamente");
+      }
     }
   };
 
@@ -82,11 +100,16 @@ export default function MalaVirtualPage() {
     <>
       <div className="flex-1 w-full bg-gradient-to-r from-[#5de0e6] to-[#004aad] flex justify-center items-center py-10 px-4">
         <div className="bg-white rounded-2xl shadow-xl w-full min-w-fit md:w-1/2 p-6 flex flex-col gap-6 text-primary">
-          <h2 className={`text-center text-xl md:text-2xl font-bold uppercase ${girassol.className}`}>
+          <h2
+            className={`text-center text-xl md:text-2xl font-bold uppercase ${girassol.className}`}
+          >
             Minha Lista de Viagem
           </h2>
 
-          <form onSubmit={handleSubmit(onSubmit)} className="gap-4 border-b-1 pb-1 border-primary">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="gap-4 border-b-1 pb-1 border-primary"
+          >
             <h3 className="font-semibold m-0 p-0">Adicionar Novo Item:</h3>
 
             <div className="flex flex-col md:flex-row gap-4">
@@ -98,7 +121,9 @@ export default function MalaVirtualPage() {
                   {...register("name")}
                 />
                 {errors.name && (
-                  <span className="text-sm text-red-500">{errors.name.message}</span>
+                  <span className="text-sm text-red-500">
+                    {errors.name.message}
+                  </span>
                 )}
               </div>
 
@@ -117,7 +142,9 @@ export default function MalaVirtualPage() {
                   <option>Alimentos</option>
                 </select>
                 {errors.category && (
-                  <span className="text-sm text-red-500">{errors.category.message}</span>
+                  <span className="text-sm text-red-500">
+                    {errors.category.message}
+                  </span>
                 )}
               </div>
             </div>
@@ -127,16 +154,19 @@ export default function MalaVirtualPage() {
               <input
                 type="number"
                 defaultValue={1}
-                
                 className="w-20 border border-gray-300 rounded-md px-3 py-2 focus:outline-none"
                 {...register("quantity")}
-                
               />
               {errors.quantity && (
-                <span className="text-sm text-red-500">{errors.quantity.message}</span>
+                <span className="text-sm text-red-500">
+                  {errors.quantity.message}
+                </span>
               )}
 
-              <LargeButton icon={<Plus color="white" />} text="Adicionar Item" />
+              <LargeButton
+                icon={<Plus color="white" />}
+                text="Adicionar Item"
+              />
             </div>
           </form>
 
@@ -147,7 +177,7 @@ export default function MalaVirtualPage() {
                 type="text"
                 placeholder="Buscar por nome/quantidade/categoria"
                 className="w-full border-0 px-4 py-2 focus:outline-none"
-                onChange={(e) => setSearchSentence(e.target.value)} 
+                onChange={(e) => setSearchSentence(e.target.value)}
               />
             </div>
           </div>
@@ -155,18 +185,22 @@ export default function MalaVirtualPage() {
           <div>
             <h3 className="font-semibold mb-2">Lista de Itens:</h3>
             <div className="flex flex-col gap-3">
-              {filteredItems.map((item) => (
-                <ItemList
-                id={item.id}
-                  key={item.id}
-                  marked={item.marked}
-                  name={item.name}
-                  quantity={item.quantity}
-                  category={item.category}
-                  onEdit={() => setEditingItem(item)}
-                  onDelete={() => setDeletingItem(item)}
-                />
-              ))}
+              {filteredItems.length > 0 ? (
+                filteredItems.map((item) => (
+                  <ItemList
+                    id={item.id}
+                    key={item.id}
+                    marked={item.marked}
+                    name={item.name}
+                    quantity={item.quantity}
+                    category={item.category}
+                    onEdit={() => setEditingItem(item)}
+                    onDelete={() => setDeletingItem(item)}
+                  />
+                ))
+              ) : (
+                <span className="text-red-400">Nenhum item corresponde</span>
+              )}
             </div>
           </div>
         </div>
